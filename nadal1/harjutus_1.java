256f971ba513f57d2a0d31fc460f7ccfc51c8908
karl-kaarel <karlmathias.fenske@gmail.com>
2022-09-04 23:28:20 +0300

    init3

---
+++ nadal1/harjutus_1.java
@@ -0,0 +1,13 @@
+/**
+ * world
+ */
+public class harjutus_1 {
+    public static void main(String[] args) {
+        String nimi = "Jane Doe";
+        System.out.println(nimi);
+    }
+
+    // public static String hello() {
+    //     return "World hello!";
+    // }
+}
---
+++ nadal1/harjutus_5.java
@@ -0,0 +1,9 @@
+public class harjutus_5 {
+    public static void main(String[] args) {
+        int year = 365;
+        int day = 24;
+        int hour = 60;
+        int seconds = 60;
+        System.out.print(year*day*hour*seconds);
+    }
+}
