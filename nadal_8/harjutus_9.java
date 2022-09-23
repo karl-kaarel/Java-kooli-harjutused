205a357eadee1b9be152e368f68f72fc92c3aab2
karl-kaarel <karlmathias.fenske@gmail.com>
2022-09-23 11:24:43 +0300

    nädal 8 / harjutus 9_3

--- nadal_8/harjutus_9.java
+++ nadal_8/harjutus_9.java
@@ -1,9 +1,7 @@
 package nadal_8;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
 import nadal_8.harjutuse_klassid.RegistrationPlate;
+import nadal_8.harjutuse_klassid.VehicleRegister;
 
 public class harjutus_9 {
     public static void main(String[] args) {
@@ -11,28 +9,14 @@ public class harjutus_9 {
         RegistrationPlate reg2 = new RegistrationPlate("FI", "UXE-465");
         RegistrationPlate reg3 = new RegistrationPlate("D", "B WQ-431");
 
-        ArrayList<RegistrationPlate> finnish = new ArrayList<RegistrationPlate>();
-        finnish.add(reg1);
-        finnish.add(reg2);
-        finnish.add(reg3);
+        VehicleRegister register = new VehicleRegister();
 
-        RegistrationPlate new_plate = new RegistrationPlate("FI", "ABC-123");
-        if (!finnish.contains(new_plate)) {
-            finnish.add(new_plate);
-        }
-        System.out.println("Finnish: " + finnish);
-        // if the equals method hasn't been overwritten, the same registration plate is repeated in the list
+        register.add(reg1, "Arto");
+        register.add(reg2, "Jürgen");
+        register.add(reg3, "Jürgen");
 
-        HashMap<RegistrationPlate, String> owners = new HashMap<RegistrationPlate, String>();
-        owners.put(reg1, "Arto");
-        owners.put(reg2, "Jürgen");
-        owners.put(reg3, "Mikk");
+        register.printRegistrationPlates();
 
-
-        System.out.println("owners:");
-        System.out.println(owners.get(reg1));
-        System.out.println(owners.get(reg2));
-        System.out.println(owners.get(reg3));
-        // if the hashCode hasn't been overwritten, the owners are not found
+        register.printOwners();
     }
 }
