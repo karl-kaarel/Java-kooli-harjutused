c4902b47de02a04cc21d20e9ecc8f7e09c36db68
karl-kaarel <karlmathias.fenske@gmail.com>
2022-10-03 19:12:34 +0300

    nadal 8 / harjutus 12.2

--- nadal_8/harjutus_12.java
+++ nadal_8/harjutus_12.java
@@ -5,12 +5,18 @@ import nadal_8.harjutuse_klassid.Storehouse;
 public class harjutus_12 {
     public static void main(String[] args) {
         Storehouse store = new Storehouse();
-        store.addProduct("milk", 3, 10);
-        store.addProduct("coffee", 5, 7);
+        store.addProduct("coffee", 5, 1);
 
-        System.out.println("prices:");
-        System.out.println("milk:  " + store.price("milk"));
-        System.out.println("coffee:  " + store.price("coffee"));
-        System.out.println("sugar: " + store.price("sugar"));
+        System.out.println("stocks:");
+        System.out.println("coffee:  " + store.stock("coffee"));
+        System.out.println("sugar: " + store.stock("sugar"));
+
+        System.out.println("we take a coffee " + store.take("coffee"));
+        System.out.println("we take a coffee " + store.take("coffee"));
+        System.out.println("we take sugar " + store.take("sugar"));
+
+        System.out.println("stocks:");
+        System.out.println("coffee:  " + store.stock("coffee"));
+        System.out.println("sugar: " + store.stock("sugar"));
     }
 }
--- nadal_8/harjutuse_klassid/Storehouse.java
+++ nadal_8/harjutuse_klassid/Storehouse.java
@@ -5,19 +5,39 @@ import java.util.Map;
 
 public class Storehouse {
     private Map<String, Integer> products;
+    private Map<String, Integer> stock;
+    
 
     //constructor
     public Storehouse() {
         products = new HashMap<String, Integer>();
+        stock = new HashMap<String, Integer>();
     }
 
     //Methods
     public void addProduct(String product, int price, int stock){
        this.products.put(product, price);
+       this.stock.put(product, stock);
     }
     public int price(String product){
         if(this.products.containsKey(product)){
             return this.products.get(product);
         }else return 0;
+    }
+
+    public Integer stock(String product) {
+        if(this.stock.containsKey(product)){
+            return this.stock.get(product);
+        }else return 0;
+    }
+
+    public Boolean take(String product) {
+        if(this.stock.containsKey(product)){
+            if (this.stock.get(product) > 0) {
+                int reducedAmount = this.stock.get(product) - 1;
+                this.stock.replace(product, reducedAmount);
+                return true;
+            } else return false;
+        }else return false;
     }
 }
