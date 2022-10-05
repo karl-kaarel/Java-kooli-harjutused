08625f4d14f95d585686ca66f2baa6f493b0faf6
karl-kaarel <karlmathias.fenske@gmail.com>
2022-10-05 23:00:43 +0300

    nadal 9 / harjutus 17.2

---
+++ nadal_9/mooc/TextUserInterface.java
@@ -0,0 +1,10 @@
+package nadal_9.mooc;
+
+import nadal_9.mooc.ui.UserInterface;
+
+public class TextUserInterface implements UserInterface {
+    @Override
+    public void update() {
+        System.out.println("Updating the user interface");
+    }
+}
