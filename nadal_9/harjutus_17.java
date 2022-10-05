1119445e43cd005ad7630097a26015ca6abe8164
karl-kaarel <karlmathias.fenske@gmail.com>
2022-10-05 23:48:05 +0300

    nadal 9 / harjutus_17.3

--- nadal_9/harjutus_17.java
+++ nadal_9/harjutus_17.java
@@ -1,7 +1,14 @@
 package nadal_9;
 
+import nadal_9.mooc.TextUserInterface;
+import nadal_9.mooc.logic.ApplicationLogic;
 import nadal_9.mooc.ui.UserInterface;
 
 public class harjutus_17 {
-    
+    public static class Main {
+        public static void main(String[] args) {
+            UserInterface ui = new TextUserInterface();
+            new ApplicationLogic(ui).execute(3);
+        }
+    }        
 }
