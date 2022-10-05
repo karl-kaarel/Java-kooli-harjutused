557f0e69f3cbc32f283a85eda3a33076569531b9
karl-kaarel <karlmathias.fenske@gmail.com>
2022-10-05 22:27:29 +0300

    nadal_9 / harjutus_17.1

---
+++ nadal_9/harjutus_17.java
@@ -0,0 +1,7 @@
+package nadal_9;
+
+import nadal_9.mooc.ui.UserInterface;
+
+public class harjutus_17 {
+    
+}
---
+++ nadal_9/mooc/ui/UserInterface.java
@@ -0,0 +1,5 @@
+package nadal_9.mooc.ui;
+
+public interface UserInterface {
+    void update();
+}
